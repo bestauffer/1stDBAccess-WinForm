5455bee6e35d3ef0b5544ef502970327b0d88dd8
bestauffer <77029920+bestauffer@users.noreply.github.com>
2022-01-10 17:45:53 -0800

    Master Commit

---
+++ Prog210-1stDBAccess-WinForm/DBaccess.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Prog210_1stDBAccess_WinForm
+{
+    public class Customer
+    {
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string City { get; set; }
+    }
+    public static class DBaccess
+    {
+        private const string connectString = @"Server=localhost; Database=northwind; Integrated Security=True";
+        public static List<Customer> GetData(string country)
+        {
+            if (country.Length < 3)
+            {
+                country = "USA";
+            }
+
+            //sets what tables we are going to look for in the database
+            SqlCommand selectCommand = new SqlCommand
+            {
+                Connection = new SqlConnection(connectString),
+                CommandText = "Select CompanyName, ContactName, Country, City from Customers WHERE Country =@Country"
+            };
+            //limits thge search to those from the country specified
+            selectCommand.Parameters.Add("@Country", SqlDbType.VarChar, 50);
+            selectCommand.Parameters["@Country"].Value = country;
+            //opens the connection
+            selectCommand.Connection.Open();
+
+            SqlDataReader sqlReader = selectCommand.ExecuteReader();
+            List<Customer> myList = new List<Customer>();
+            while (sqlReader.Read()) // or can use the column names directly
+            {
+                Customer anotherOne = new Customer
+                {
+                    CompanyName = sqlReader["CompanyName"].ToString(),
+                    ContactName = sqlReader["ContactName"].ToString(),
+                    City = sqlReader["City"].ToString()
+                };
+                myList.Add(anotherOne);
+            }
+
+            sqlReader.Close();
+            selectCommand.Connection.Close();
+            return myList;
+        }
+    }
+}
---
+++ Prog210-1stDBAccess-WinForm/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Prog210_1stDBAccess_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // prepare and format the listView
+            listView1.BackColor = Color.LightBlue;  // just playing with the listview props and appearance
+            listView1.ForeColor = Color.Blue;
+            listView1.BorderStyle = BorderStyle.Fixed3D;
+            listView1.View = View.Details;  // just like file explorer, there are several view options
+            listView1.Columns.Add("CompanyName", 200, HorizontalAlignment.Left);  // these numbers are pixels, not characters
+            listView1.Columns.Add("ContactName", 150, HorizontalAlignment.Left);
+            listView1.Columns.Add("City", 150, HorizontalAlignment.Left);
+            listView1.GridLines = true;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string myCountry = textBox1.Text;
+            listView1.Items.Clear();
+            List<Customer> formsList = DBaccess.GetData(myCountry);
+            for (int i = 0; i < formsList.Count; i++)
+            {
+                listView1.Items.Add(formsList[i].CompanyName);
+                listView1.Items[listView1.Items.Count - 1].SubItems.Add(formsList[i].CompanyName);
+                listView1.Items[listView1.Items.Count - 1].SubItems.Add(formsList[i].City);
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
